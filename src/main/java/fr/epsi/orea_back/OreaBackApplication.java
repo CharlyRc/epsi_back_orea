f8275a2bee4f9eaeaa7cc7ffe93107e1ed0c19c5
Charly RC <ricoul.charly@orange.fr>
2024-04-29 12:12:51 +0200

    test

---
+++ src/main/java/fr/epsi/orea_back/OreaBackApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.orea_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OreaBackApplication.class, args);
+    }
+
+}
